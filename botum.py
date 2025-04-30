59aeb2d0667963c5155115a836bb6a2c4ab2b542
yektabayram <yektabayram6984@gmail.com>
2025-04-30 19:51:06 +0300

    Update botum.py

--- botum.py
+++ botum.py
@@ -1,10 +1,11 @@
 import discord
 from discord.ext import commands
-
+import os
+import random
 intents = discord.Intents.default()
 intents.message_content = True
 
-bot = commands.Bot(command_prefix='$hello', intents=intents)
+bot = commands.Bot(command_prefix='$', intents=intents)
 
 @bot.event
 async def on_ready():
@@ -17,19 +18,46 @@ async def hello(ctx):
 @bot.command()
 async def heh(ctx,count_heh=5):
    await ctx.send("he"*count_heh) 
+
 @bot.command()
 async def add(ctx, left: int, right: int):
     """Adds two numbers together."""
     await ctx.send(left + right)  
+
 @bot.command()
 async def repeat(ctx, times: int, content='repeating...'):
     """Repeats a message multiple times."""
     for i in range(times):
         await ctx.send(content)
+        
 @bot.command(description='For when you wanna settle the score some other way')
 async def choose(ctx, *choices: str):
     """Chooses between multiple choices."""
-    await ctx.send(random.choice(choices)
+    await ctx.send(random.choice(choices))
 
+@bot.command()
+async def mem(ctx):
+    resim = random.choice(os.listdir('images'))
+    # Dosya adını bir değişkenden bu şekilde değiştirebilirsiniz!
+    with open(f'images/{resim}', 'rb') as f:
+        picture = discord.File(f)
+   # Daha sonra bu dosyayı bir parametre olarak gönderebiliriz!
+    await ctx.send(file=picture)
 
-bot.run("")
+oneriler=["plastik atıklarınızla el işi yapın","tekrar poşetleri kullanabilirsiniz","pilleria atık kutusuna atabiliriz"]
+
+@bot.command()
+async def atik(ctx):
+    sonuc=random.choice(oneriler)
+    await ctx.send(sonuc)
+
+@bot.command()
+async def afis(ctx):
+    afis = random.choice(os.listdir('afisler'))
+    # Dosya adını bir değişkenden bu şekilde değiştirebilirsiniz!
+    with open(f'afisler/{afis}', 'rb') as f:
+        afis_ = discord.File(f)
+   # Daha sonra bu dosyayı bir parametre olarak gönderebiliriz!
+    await ctx.send(file=afis_)
+
+bot.run("TOKEN")
